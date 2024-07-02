2a520eca0afe41906a50630b4a34848cc5e1e146
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-02 15:26:13 +0900

    <docs>: <에러, 메인 페이지 추가>
    - 기본적인 경로설정을 위해 메인페이지와 에러페이지를 추가했습니다.

---
+++ study-badge/src/pages/Error.tsx
@@ -0,0 +1,8 @@
+const Error = (): JSX.Element => {
+  // 페이지 폴더 컴포넌트들은 여기에 로직 없이
+  // return에 컴포넌트들을 담아 페이지에 보여주는 용으로만 사용하기로 해요!
+  // 이렇게 해야 나중에 해당 페이지에 속한 컴포넌트를 금방 찾아갈 수 있습니다!
+  return <div>에러 페이지</div>;
+};
+
+export default Error;
