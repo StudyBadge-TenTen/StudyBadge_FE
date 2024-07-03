586fa58af2ac2d4239f4bc6e1961bc5d56d82ce9
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-03 16:57:17 +0900

    <feat>: <Header 레이아웃 구현>
    - Header 컴포넌트를 생성해 App 컴포넌트에 삽입했습니다.
    - 기본적인 레이아웃만 구현된 상태입니다.

--- study-badge/src/App.tsx
+++ study-badge/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <BrowserRouter>
       {/* Header(Nav) 컴포넌트 삽입 예정 */}
+      <Header />
       <section className="main min-h-96 flex justify-center items-center">
-        <div className="w-[80vw] min-h-96 flex justify-center items-center">
+        <div className="w-[1025px] min-h-96 flex justify-center items-center">
           <Router />
         </div>
       </section>
