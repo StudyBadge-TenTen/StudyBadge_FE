250e40b09a63eaaf8b1b8450d155cc7b74837a15
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-03 23:27:07 +0900

    <feat>: <스터디 채널 카드북 레이아웃 구현>
    - 스터디 채널 카드북 레이아웃 구현

---
+++ study-badge/src/components/channel/ChannelBook.tsx
@@ -0,0 +1,12 @@
+const ChannelBook = (): JSX.Element => {
+  return (
+    <>
+      <div className="container w-fit min-w-80 h-[80vh] rounded-[50px] shadow-card">
+        <div className="h-[15%] bg-[#1C4587] rounded-t-[50px]"></div>
+        <div className="h-[85%] p-8"></div>
+      </div>
+    </>
+  );
+};
+
+export default ChannelBook;
