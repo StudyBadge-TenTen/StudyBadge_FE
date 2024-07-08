6d4f207e5d2e5cfef6a3f452beee6157b09405b0
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-08 17:47:09 +0900

    <docs>: <데이터 구조 변경에 따른 코드 수정>
    - 일정 데이터 구조 수정이 있었습니다
    - 서버에 최대한 null값이 저장되는 것을 지양하고자
    - null이 없는 쪽으로 구현하기로 했고,
    - 전체적으로 타입 수정과 로직을 수정했습니다.

--- src/types/schedule-type.ts
+++ src/types/schedule-type.ts
@@ -1,5 +1,38 @@
-type RepeatDailyType = "DAILY_ONE" | "DAILY_TWO" | "DAILY_THREE" | "DAILY_FOUR" | "DAILY_FIVE" | "DAILY_SIX";
+type RepeatCycleType = "DAILY" | "WEEKLY" | "MONTHLY";
+type RepeatDailyType = "EVERYDAY";
 type RepeatWeeklyType = "SUNDAY" | "MONDAY" | "TUESDAY" | "WEDNESDAY" | "THURSDAY" | "FRIDAY" | "SATURDAY";
+type RepeatMonthlyType =
+  | "MONTHLY_ONE"
+  | "MONTHLY_TWO"
+  | "MONTHLY_THREE"
+  | "MONTHLY_FOUR"
+  | "MONTHLY_FIVE"
+  | "MONTHLY_SIX"
+  | "MONTHLY_SEVEN"
+  | "MONTHLY_EIGHT"
+  | "MONTHLY_NINE"
+  | "MONTHLY_TEN"
+  | "MONTHLY_ELEVEN"
+  | "MONTHLY_TWELVE"
+  | "MONTHLY_THIRTEEN"
+  | "MONTHLY_FOURTEEN"
+  | "MONTHLY_FIFTEEN"
+  | "MONTHLY_SIXTEEN"
+  | "MONTHLY_SEVENTEEN"
+  | "MONTHLY_EIGHTEEN"
+  | "MONTHLY_NINETEEN"
+  | "MONTHLY_TWENTY"
+  | "MONTHLY_TWENTY_ONE"
+  | "MONTHLY_TWENTY_TWO"
+  | "MONTHLY_TWENTY_THREE"
+  | "MONTHLY_TWENTY_FOUR"
+  | "MONTHLY_TWENTY_FIVE"
+  | "MONTHLY_TWENTY_SIX"
+  | "MONTHLY_TWENTY_SEVEN"
+  | "MONTHLY_TWENTY_EIGHT"
+  | "MONTHLY_TWENTY_NINE"
+  | "MONTHLY_THIRTY"
+  | "MONTHLY_THIRTY_ONE";
 
 interface ScheduleType {
   id: number | string;
@@ -9,10 +42,10 @@ interface ScheduleType {
   scheduleDate: "YYYY-MM-DD" | string;
   scheduleStartTime: "00:00:00" | string;
   scheduleEndTime: "00:00:00" | string;
-  repeatCycle: null | "DAILY" | "WEEKLY" | "MONTHLY";
-  repeatSituation: null | RepeatDailyType | RepeatWeeklyType;
-  repeatEndDate: "YYYY-MM-DD" | string;
-  placeId: number | string;
+  repeatCycle?: RepeatCycleType;
+  repeatSituation?: RepeatDailyType | RepeatWeeklyType | RepeatMonthlyType;
+  repeatEndDate?: "YYYY-MM-DD" | string;
+  placeId?: number | string;
   repeated: boolean;
 }
 
@@ -47,7 +80,7 @@ interface ScheduleInfoType {
   name: string;
   content: string;
   time: string[];
-  placeAddress: string;
+  placeAddress?: string;
 }
 
 // -------- 임시) 장소 관련 ----------
--- src/utils/schedule-function.ts
+++ src/utils/schedule-function.ts
@@ -1,76 +1,38 @@
 // 1.DAILY 2.WEEKLY 3.MONTHLY 3가지 상황 날짜 계산해서 마크 리스트로 반환하는 함수
 
 import moment from "moment";
-import { RepeatDailyType, ScheduleCalcResponseType, ScheduleParamsType, ScheduleType } from "../types/schedule-type";
+import { ScheduleCalcResponseType, ScheduleParamsType, ScheduleType } from "../types/schedule-type";
 import { getPlace, getSchedules } from "../services/schedule-api";
 
-// 일간반복(며칠마다)으로 달력에 마크할 날짜들을 반환하는 함수
-const repeatDaily = (
+const repeatFunction = (
   scheduleDate: ScheduleType["scheduleDate"],
-  repeatSituation: RepeatDailyType,
+  repeatCycle: ScheduleType["repeatCycle"],
   repeatEndDate: ScheduleType["repeatEndDate"],
 ) => {
   const startDate = new Date(scheduleDate);
   let newDate = startDate;
   let marks = [moment(newDate).format("YYYY-MM-DD")];
 
-  switch (repeatSituation) {
-    case "DAILY_ONE":
-      while (new Date(newDate.setDate(newDate.getDate() + 1)) <= new Date(repeatEndDate)) {
-        marks.push(moment(newDate).format("YYYY-MM-DD"));
-      }
-      break;
-    case "DAILY_TWO":
-      while (new Date(newDate.setDate(newDate.getDate() + 2)) <= new Date(repeatEndDate)) {
-        marks.push(moment(newDate).format("YYYY-MM-DD"));
-      }
-      break;
-    case "DAILY_THREE":
-      while (new Date(newDate.setDate(newDate.getDate() + 3)) <= new Date(repeatEndDate)) {
-        marks.push(moment(newDate).format("YYYY-MM-DD"));
-      }
-      break;
-    case "DAILY_FOUR":
-      while (new Date(newDate.setDate(newDate.getDate() + 4)) <= new Date(repeatEndDate)) {
-        marks.push(moment(newDate).format("YYYY-MM-DD"));
-      }
-      break;
-    case "DAILY_FIVE":
-      while (new Date(newDate.setDate(newDate.getDate() + 5)) <= new Date(repeatEndDate)) {
-        marks.push(moment(newDate).format("YYYY-MM-DD"));
-      }
-      break;
-    case "DAILY_SIX":
-      while (new Date(newDate.setDate(newDate.getDate() + 6)) <= new Date(repeatEndDate)) {
-        marks.push(moment(newDate).format("YYYY-MM-DD"));
-      }
-      break;
-  }
-
-  return marks;
-};
-
-// 주간반복(매주)으로 달력에 마크할 날짜들을 반환하는 함수
-const repeatWeekly = (scheduleDate: ScheduleType["scheduleDate"], repeatEndDate: ScheduleType["repeatEndDate"]) => {
-  const startDate = new Date(scheduleDate);
-  let newDate = startDate;
-  let marks = [moment(newDate).format("YYYY-MM-DD")];
-
-  while (new Date(newDate.setDate(newDate.getDate() + 7)) <= new Date(repeatEndDate)) {
-    marks.push(moment(newDate).format("YYYY-MM-DD"));
-  }
-
-  return marks;
-};
-
-// 월간반복(매월)으로 달력에 마크할 날짜들을 반환하는 함수
-const repeatMonthly = (scheduleDate: ScheduleType["scheduleDate"], repeatEndDate: ScheduleType["repeatEndDate"]) => {
-  const startDate = new Date(scheduleDate);
-  const newDate = startDate;
-  let marks = [moment(newDate).format("YYYY-MM-DD")];
-
-  while (new Date(newDate.setMonth(newDate.getMonth() + 1)) <= new Date(repeatEndDate)) {
-    marks.push(moment(newDate).format("YYYY-MM-DD"));
+  if (repeatEndDate) {
+    switch (repeatCycle) {
+      case "DAILY":
+        while (new Date(newDate.setDate(newDate.getDate() + 1)) <= new Date(repeatEndDate)) {
+          marks.push(moment(newDate).format("YYYY-MM-DD"));
+        }
+        break;
+      case "WEEKLY":
+        while (new Date(newDate.setDate(newDate.getDate() + 7)) <= new Date(repeatEndDate)) {
+          marks.push(moment(newDate).format("YYYY-MM-DD"));
+        }
+        break;
+      case "MONTHLY":
+        while (new Date(newDate.setMonth(newDate.getMonth() + 1)) <= new Date(repeatEndDate)) {
+          marks.push(moment(newDate).format("YYYY-MM-DD"));
+        }
+        break;
+    }
+  } else {
+    throw new Error("반복 종료일 설정이 되어있지 않습니다.");
   }
 
   return marks;
@@ -84,22 +46,7 @@ const scheduleCalculator = async ({ channelId, year, month }: ScheduleParamsType
       let marks: string[] = [];
 
       if (schedule.repeated) {
-        // 반복타입에 따라 4가지 함수 호출+계산
-        switch (schedule.repeatCycle) {
-          case "DAILY":
-            marks = repeatDaily(
-              schedule.scheduleDate,
-              schedule.repeatSituation as RepeatDailyType,
-              schedule.repeatEndDate,
-            );
-            break;
-          case "WEEKLY":
-            marks = repeatWeekly(schedule.scheduleDate, schedule.repeatEndDate);
-            break;
-          case "MONTHLY":
-            marks = repeatMonthly(schedule.scheduleDate, schedule.repeatEndDate);
-            break;
-        }
+        marks = repeatFunction(schedule.scheduleDate, schedule.repeatCycle, schedule.repeatEndDate);
       } else marks = [schedule.scheduleDate];
 
       return { scheduleId: schedule.id, marks: marks };
@@ -128,17 +75,25 @@ const getScheduleInfo = async (
   const scheduleObject = scheduleList.find((schedule) => schedule.id === scheduleId);
 
   if (scheduleObject) {
-    const params = { scheduleId: scheduleObject.id, placeId: scheduleObject.placeId };
-    const { placeAddress } = await getPlace(params);
-    scheduleInfo = {
-      name: scheduleObject.scheduleName,
-      content: scheduleObject.scheduleContent,
-      time: [scheduleObject.scheduleStartTime, scheduleObject.scheduleEndTime],
-      placeAddress: placeAddress,
-    };
+    if (scheduleObject.placeId) {
+      const params = { scheduleId: scheduleObject.id, placeId: scheduleObject.placeId };
+      const { placeAddress } = await getPlace(params);
+      scheduleInfo = {
+        name: scheduleObject.scheduleName,
+        content: scheduleObject.scheduleContent,
+        time: [scheduleObject.scheduleStartTime, scheduleObject.scheduleEndTime],
+        placeAddress: placeAddress,
+      };
+    } else {
+      scheduleInfo = {
+        name: scheduleObject.scheduleName,
+        content: scheduleObject.scheduleContent,
+        time: [scheduleObject.scheduleStartTime, scheduleObject.scheduleEndTime],
+      };
+    }
   }
 
   return { result, scheduleInfo };
 };
 
-export { repeatDaily, repeatWeekly, repeatMonthly, scheduleCalculator, getScheduleInfo };
+export { repeatFunction, scheduleCalculator, getScheduleInfo };
