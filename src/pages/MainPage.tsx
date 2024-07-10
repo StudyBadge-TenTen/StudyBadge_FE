19bb976e9a04be1eeaa16e5896cda6db8d1909cf
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-10 23:26:55 +0900

    <docs>: <메인페이지 레이아웃 구현 마무리>
    - css 색깔 변수로 Red가 추가되었습니다.
    - 채널 리스트 필터링 부분에 반응형 디자인을 구현했습니다.
    - Card 컴포넌트 추가 : 채널 카드 하나를 렌더링하는 컴포넌트입니다.
    - 일단 api로 스터디채널리스트를 가져올 때 받을 정보를
    - Card에서 props로 받아 화면에 렌더링 하는 식으로 틀을 잡았습니다.

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -6,7 +6,7 @@ const Main = (): JSX.Element => {
   // 이렇게 해야 나중에 해당 페이지에 속한 컴포넌트를 금방 찾아갈 수 있습니다!
   return (
     <div className="w-full h-fit flex flex-col justify-center items-center">
-      <div className="carousel w-full min-h-40 max-h-96 bg-Gray-1 flex justify-center items-center mb-10">carousel</div>
+      <div className="carousel w-full h-96 bg-Gray-1 flex justify-center items-center my-20">carousel</div>
       <div className="create-study w-full h-96 flex flex-col sm:flex-row px-8">
         <div className="w-full sm:w-1/2 h-full flex flex-col justify-center items-center sm:items-start text-Blue-2">
           <p className="text-2xl sm:text-3xl font-bold mb-4">스터디 그룹을 생성하세요!</p>
@@ -19,34 +19,64 @@ const Main = (): JSX.Element => {
           image
         </div>
       </div>
-      <div className="study-list w-full px-8 my-20 flex flex-col justify-center items-center">
-        <div className="text-3xl font-bold text-Blue-2 mb-10">스터디 채널</div>
-        <div className="w-full flex flex-col md:flex-row justify-between items-center mb-10">
-          <div className="flex items-center">
-            <span>방식</span>
-            <button className="btn-blue rounded-r-none text-xs md:text-sm break-keep ml-2">오프라인</button>
-            <button className="btn-blue rounded-l-none text-xs md:text-sm break-keep ml-1">온라인</button>
+      <div className="study-list w-full px-8 my-20 mt-40 flex flex-col justify-center items-center">
+        <h2 className="text-3xl font-bold text-Blue-2 mb-10">스터디 채널</h2>
+        <div className="w-full flex flex-col lg:flex-row justify-between items-center mb-20">
+          <div className="flex flex-col justify-center items-center">
+            <span className="text-Gray-4">방식</span>
+            <div className="flex items-center mt-2">
+              <button id="offlineFilter" className="btn-blue rounded-r-none text-xs md:text-sm break-keep ml-2">
+                오프라인
+              </button>
+              <button id="onlineFilter" className="btn-blue rounded-l-none text-xs md:text-sm break-keep ml-1">
+                온라인
+              </button>
+            </div>
             {/* 아래 오프라인 클릭 시 지역 선택 드롭다운 렌더링할 코드 */}
             <div></div>
           </div>
-          <div className="flex items-center">
-            <span>상태</span>
-            <button className="btn-blue rounded-r-none text-xs md:text-sm break-keep ml-2">모집중</button>
-            <button className="btn-blue rounded-l-none text-xs md:text-sm break-keep ml-1">모집완료</button>
-            {/* 아래 오프라인 클릭 시 지역 선택 드롭다운 렌더링할 코드 */}
-            <div></div>
+          <div className="flex flex-col justify-center items-center">
+            <span className="text-Gray-4">상태</span>
+            <div className="flex items-center mt-2">
+              <button id="recruitingFilter" className="btn-blue rounded-r-none text-xs md:text-sm break-keep ml-2">
+                모집중
+              </button>
+              <button id="recruitedFilter" className="btn-blue rounded-l-none text-xs md:text-sm break-keep ml-1">
+                모집완료
+              </button>
+            </div>
           </div>
-          <div className="flex items-center mt-4 md:mt-0">
-            <span>카테고리</span>
-            <button className="btn-blue rounded-r-none text-xs md:text-sm break-keep ml-2">컴퓨터/IT/개발</button>
-            <button className="btn-blue rounded-none text-xs md:text-sm break-keep ml-1">언어/어학</button>
-            <button className="btn-blue rounded-none text-xs md:text-sm break-keep ml-1">취업/이직</button>
-            <button className="btn-blue rounded-l-none text-xs md:text-sm break-keep ml-1">자기계발</button>
+          <div className="flex flex-col justify-center items-center mt-4 md:mt-0">
+            <span className="text-Gray-4">카테고리</span>
+            <div className="flex flex-wrap sm:flex-nowrap justify-center items-center mt-2">
+              <button id="computerCategory" className="btn-blue sm:rounded-r-none text-xs md:text-sm break-keep ml-2">
+                컴퓨터/IT/개발
+              </button>
+              <button id="languageCategory" className="btn-blue sm:rounded-none text-xs md:text-sm break-keep ml-1">
+                언어/어학
+              </button>
+              <button id="employmentCategory" className="btn-blue sm:rounded-none text-xs md:text-sm break-keep ml-1">
+                취업/이직
+              </button>
+              <button
+                id="selfDevelopCategory"
+                className="btn-blue sm:rounded-l-none text-xs md:text-sm break-keep ml-1 mt-2 sm:mt-0"
+              >
+                자기계발
+              </button>
+            </div>
           </div>
         </div>
-        <div className="study-cards-container w-full flex flex-wrap">
-          <Card />
+        <div className="study-cards-container w-full flex justify-center items-center flex-wrap">
+          {/* 받은 채널 리스트의 길이만큼 map을 이용해 Card 생성 */}
+          <Card studyInfo={""} />
+          <Card studyInfo={""} />
+          <Card studyInfo={""} />
+          <Card studyInfo={""} />
+          <Card studyInfo={""} />
+          <Card studyInfo={""} />
         </div>
+        {/* 페이지네이션 컴포넌트 */}
       </div>
     </div>
   );
