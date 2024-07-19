e5bdb37b39f4fc5815574653410c9a417821cc5d
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-20 03:03:03 +0900

    <docs>: <로그인 페이지 레이아웃 수정>
    - 로그인 페이지 레이아웃을 수정했습니다.

--- src/components/layout/login/NaverLogin.tsx
+++ src/components/layout/login/NaverLogin.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NAVER_ICON from "../../../assets/social/NAVER_ICON.png";
 // import { useNavigate } from 'react-router-dom';
 
 const NaverLogin: React.FC = () => {
   // const navigate = useNavigate();
-  const clientId = 'VITE_APP_NAVER_LOGIN_CLIENT_KEY';
+  const clientId = "VITE_APP_NAVER_LOGIN_CLIENT_KEY";
   const redirectUri = encodeURIComponent("./auth/naver/NaverLoginCallback");
 
   const handleNaverLogin = () => {
@@ -12,7 +13,11 @@ const NaverLogin: React.FC = () => {
   };
 
   return (
-    <button onClick={handleNaverLogin} className="w-full max-w-80 btn-blue text-white py-2 rounded mt-4">
+    <button
+      onClick={handleNaverLogin}
+      className="w-[183px] md:w-[300px] btn-blue bg-[#00C73C] hover:bg-[#00C73C] text-white p-0 py-1 rounded mt-4 flex justify-center items-center"
+    >
+      <img src={NAVER_ICON} className="w-8" />
       네이버 로그인
     </button>
   );
