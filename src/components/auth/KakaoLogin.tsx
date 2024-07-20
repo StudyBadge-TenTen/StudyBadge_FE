5ab256ecdb8a21d6ee200ae126d4ed14e7625064
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-21 01:22:06 +0900

    <docs>: <카카오 소셜 로그인 코드 수정>

--- src/components/auth/KakaoLogin.tsx
+++ src/components/auth/KakaoLogin.tsx
@@ -3,14 +3,20 @@ import KAKAO_BTN_SMALL from "../../assets/social/KAKAO_BTN_SMALL.png";
 import KAKAO_BTN_BIG from "../../assets/social/KAKAO_BTN_BIG.png";
 
 const KakaoLogin: React.FC = () => {
-  const CLIENT_ID = import.meta.env.VITE_APP_KAKAO_LOGIN_CLIENT_KEY; // 실제 카카오 클라이언트 ID로 교체 필요
-  const REDIRECT_URL = import.meta.env.DEV
-    ? import.meta.env.VITE_APP_KAKAO_REDIRECT_URL_DEV
-    : import.meta.env.VITE_APP_KAKAO_REDIRECT_URL_PROD;
+  // 서버 없을 시
+  // const CLIENT_ID = import.meta.env.VITE_APP_KAKAO_LOGIN_CLIENT_KEY; // 실제 카카오 클라이언트 ID로 교체 필요
+  // const REDIRECT_URL = import.meta.env.DEV
+  //   ? import.meta.env.VITE_APP_KAKAO_REDIRECT_URL_DEV
+  //   : import.meta.env.VITE_APP_KAKAO_REDIRECT_URL_PROD;
+
+  const URL = import.meta.env.DEV ? import.meta.env.VITE_APP_LOCAL_HOST : import.meta.env.VITE_APP_PRODUCTION_URL;
 
   const handleKakaoLogin = () => {
-    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
-    window.location.href = kakaoAuthUrl;
+    // 서버 없을 시
+    // const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&response_type=code`;
+    // window.location.href = kakaoAuthUrl;
+
+    window.location.href = `${URL}/oauth2/authorization/naver`;
   };
 
   return (
