24e5e593508b8e62396a1a8731961c00274cfb10
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-22 00:15:44 +0900

    <fix>: <SSE 코드 수정>
    - SSE 요청 시 토큰 여부가 불규칙적인 현상을 디버깅해보고자
    - 토큰이 저장이 안되어있을 시, 로직을 수행하지 않도록 막는 코드를 삽입
    - 따로 함수들을 등록해 한꺼번에 실행하고 의존성배열에 몇 가지 추가

--- src/hooks/useSSE.tsx
+++ src/hooks/useSSE.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useAuthStore } from "../store/auth-store";
 import { useNotificationStore } from "../store/notification-store";
-import { EventSourcePolyfill } from "event-source-polyfill";
+import { Event, EventSourcePolyfill, MessageEvent } from "event-source-polyfill";
 import { NotificationType } from "../types/notification-type";
 import { LAST_EVENT_ID } from "../constants/local-storage";
 
@@ -10,9 +10,11 @@ export const useSSE = () => {
   const API_BASE_URL = import.meta.env.DEV ? "/" : import.meta.env.VITE_APP_TEST_BASE_URL;
   const { notifications, setNotifications } = useNotificationStore();
 
-  console.log("useSSE hook"); // useSSE작동 테스트
+  console.log("useSSE hook"); // useSSE 작동 테스트
 
   useEffect(() => {
+    if (!accessToken) return;
+
     const eventSource = new EventSourcePolyfill(API_BASE_URL + `api/subscribe`, {
       headers: {
         "Last-Event-ID": localStorage.getItem(LAST_EVENT_ID) ?? "",
@@ -22,35 +24,42 @@ export const useSSE = () => {
       withCredentials: true,
     });
 
-    const connect = () => {
-      if (!accessToken) return;
-
-      eventSource.onmessage = (e) => {
-        console.log("Event received: ", e.data); // 디버깅 로그 추가
-        try {
-          const newNotification: NotificationType = JSON.parse(e.data);
-          console.log("Parsed notification: ", newNotification); // 디버깅 로그 추가
-          setNotifications([...notifications, newNotification]);
-          localStorage.setItem(LAST_EVENT_ID, e.lastEventId);
-        } catch (error) {
-          console.error("Failed to parse event data: ", error);
-        }
-      };
-
-      eventSource.onerror = (err) => {
-        console.log("Error receiving notifications", err);
-        // setError();
-        eventSource.close();
-        // 재연결
-        setTimeout(connect, 3000);
-      };
-
-      eventSource.onopen = () => {
-        console.log("SSE 연결");
-      };
+    const onMessage = (ev: MessageEvent) => {
+      console.log("Event received: ", ev.data); // 디버깅 로그 추가
+      try {
+        const newNotification: NotificationType = JSON.parse(ev.data);
+        console.log("Parsed notification: ", newNotification); // 디버깅 로그 추가
+        setNotifications([...notifications, newNotification]);
+        localStorage.setItem(LAST_EVENT_ID, ev.lastEventId);
+      } catch (error) {
+        console.error("Failed to parse event data: ", error);
+      }
     };
+
+    const onError = (err: Event) => {
+      console.log("Error receiving notifications", err);
+      // setError();
+      eventSource.close();
+      // 재연결
+      setTimeout(connect, 3000);
+    };
+
+    const onOpen = () => {
+      console.log("SSE 연결");
+    };
+
+    const connect = () => {
+      eventSource.onmessage = onMessage;
+      eventSource.onerror = onError;
+      eventSource.onopen = onOpen;
+    };
+
     connect();
 
-    return () => eventSource.close();
-  }, [accessToken]);
+    return () => {
+      eventSource.close();
+    };
+  }, [accessToken, API_BASE_URL, setNotifications]);
+
+  return null;
 };
