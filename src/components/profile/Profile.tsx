14da3032bfffb54ffec020ab7b381f4059bd82b6
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-22 10:59:11 +0900

    <fix>: <헤더 로그인/로그아웃 버튼 css 수정>
    - css가 제대로 적용이 되지 않는 것 같아 수정했습니다.
    - 프로필 스켈레톤을 적용했습니다.

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -19,7 +19,7 @@ const Profile = (): JSX.Element => {
   const [chargeAmount, setChargeAmount] = useState(10000);
   const [myStudy, setMyStudy] = useState<MyStudyType[]>([]);
   const { isEditMode, setIsEditMode } = useEditModeStore();
-  const { data } = useQuery<UserInfoType, Error>({
+  const { data, isLoading, error } = useQuery<UserInfoType, Error>({
     queryKey: ["UserInfo"],
     queryFn: () => getProfile(),
   });
@@ -52,7 +52,26 @@ const Profile = (): JSX.Element => {
 
   return (
     <>
-      {isEditMode ? (
+      {isLoading && (
+        <div className="w-full min-h-52 border border-solid border-Gray-3 rounded-t-[30px] p-6 flex flex-col items-center md:flex-row">
+          <div className="flex items-center">
+            <div className="no-profile-image w-32 h-32 bg-Gray-2 rounded-full flex justify-center items-center mr-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="80"
+                height="80"
+                fill="white"
+                className="bi bi-person"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+              </svg>
+            </div>
+            <div className="w-32 h-12 rounded-[30px] bg-Gray-1"></div>
+          </div>
+        </div>
+      )}
+      {data && isEditMode ? (
         <ProfileEdit />
       ) : (
         <>
@@ -131,6 +150,11 @@ const Profile = (): JSX.Element => {
             ))
           )}
         </>
+      )}
+      {error && (
+        <div>
+          프로필을 로딩하는데 실패했습니다. errorName: {error.name}. errorMessage: {error.message}
+        </div>
       )}
     </>
   );
