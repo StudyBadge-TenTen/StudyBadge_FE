728ef79cf4dcb1f9a7d25bccddd1c040e84946c0
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-22 23:25:32 +0900

    <fix>: <sse 재요청 코드로 인해 계속 쿼리 요청되는 현상 디버깅>
    - 변수 retryTimeout을 따로 지정하고 timeout코드를 할당해주어
    - 해당 timeout을 clear하는 코드를 추가하였습니다.
    - 추후에 더미 데이터를 거르는 코드를 삽입할 예정입니다.

--- src/hooks/useSSE.tsx
+++ src/hooks/useSSE.tsx
@@ -17,6 +17,7 @@ export const useSSE = () => {
 
     // 에러 시 재연결을 시도하면 새로운 EventSourcePolyfill객체가 생성되도록하기 위해 이렇게 작성했습니다.
     let eventSource: EventSourcePolyfill | null = null;
+    let retryTimeout: NodeJS.Timeout | null = null; // 메인페이지에서 계속 쿼리가 실행되는 것 디버깅하기 위한 코드
 
     const connect = () => {
       eventSource = new EventSourcePolyfill(API_BASE_URL + `api/notifications/subscribe`, {
@@ -24,7 +25,7 @@ export const useSSE = () => {
           "Last-Event-ID": localStorage.getItem(LAST_EVENT_ID) ?? "",
           Authorization: `Bearer ${accessToken}`,
         },
-        heartbeatTimeout: 30000, // 30초가 더 안전한 것 같아 30초로 수정해뒀습니다
+        heartbeatTimeout: 120000, // 서버와 동일하게 설정하는 것이 맞다고 함
         withCredentials: true,
       });
 
@@ -35,6 +36,7 @@ export const useSSE = () => {
 
     const onMessage = (ev: MessageEvent) => {
       console.log("Event received: ", ev.data); // 디버깅 로그 추가
+      // 추가해야할 코드 : 더미 데이터일 경우 return
       try {
         const newNotification: NotificationType = JSON.parse(ev.data);
         console.log("Parsed notification: ", newNotification); // 디버깅 로그 추가
@@ -50,8 +52,11 @@ export const useSSE = () => {
       if (eventSource) {
         eventSource.close();
       }
+      if (retryTimeout) {
+        clearTimeout(retryTimeout); // 클리어하는 코드 추가
+      }
       // 재연결
-      setTimeout(connect, 3000);
+      retryTimeout = setTimeout(connect, 3000);
     };
 
     const onOpen = () => {
@@ -63,6 +68,9 @@ export const useSSE = () => {
     return () => {
       if (eventSource) {
         eventSource.close();
+      }
+      if (retryTimeout) {
+        clearTimeout(retryTimeout); // 클리어하는 코드 추가
       }
     };
   }, [accessToken, API_BASE_URL, setNewNotification]);
