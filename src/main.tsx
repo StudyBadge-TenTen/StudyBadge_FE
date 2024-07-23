a732974366aaa660545492b0276f9cb09be25614
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-23 12:19:42 +0900

    <docs>: <develop용 코드 주석 해제>

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { worker } from "./mocks/worker.ts";
+import { worker } from "./mocks/worker.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// if (import.meta.env.DEV) {
-//   worker.start();
-// }
+if (import.meta.env.DEV) {
+  worker.start();
+}
 
 const queryClient = new QueryClient();
 
