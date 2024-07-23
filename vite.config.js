ba0124bd90ddd77f9e6ecb384e29ddf53b50aecd
nageuna922@gmail.com <nageuna922@gmail.com>
2024-07-24 00:13:50 +0900

    <chore>: <typescript 관련 플러그인 설치 및 디버깅>
    - tsconfig.json파일에 참조 에러가 발생하여 디버깅을 시도
    - vite-tsconfig-paths 플러그인을 설치
    - 빌드 설정과 참조된 프로젝트가 emit(파일 출력)을 비활성화할 수 없는 문제
    - 해당 부분 디버깅 : noEmit false로 변경
    - allowImportingTsExtensions 옵션은 noEmit 또는 emitDeclarationOnly 옵션이 활성화된 경우에만 사용할 수 있음
    - emitDeclarationOnly를 설정

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: { find: "@", replacement: resolve(__dirname, "src") },
+    },
+    plugins: [react(), tsconfigPaths()],
+    test: {
+        environment: "jsdom",
+    },
+    // server: {
+    //   proxy: {
+    //     "/api": {
+    //       target: "http://localhost:8080", // 안될 경우 'http://127.0.0.1:8000', 이런식으로 IP 명시
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { test } from "vitest";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: { find: "@", replacement: resolve(__dirname, "src") },
+  },
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: "jsdom",
   },
