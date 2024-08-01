b7ff886df7e8cf1322c94bc14577ec022f7740d0
nageuna922@gmail.com <nageuna922@gmail.com>
2024-08-01 17:27:34 +0900

    <fix>: <로그인 시 에러창 2번 뜨는 현상>

--- src/components/auth/LoginUser.tsx
+++ src/components/auth/LoginUser.tsx
@@ -32,7 +32,7 @@ const LoginUser: React.FC = () => {
       // window.location.reload();
     } catch (error) {
       // console.error("로그인 실패:", error);
-      alert("이메일 또는 비밀번호가 올바르지 않습니다.");
+      console.log(error);
     }
   };
 
