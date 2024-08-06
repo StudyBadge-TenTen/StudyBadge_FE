e443076bb7e3c58ebbb857a3be5f8d1004bc4283
nageuna922@gmail.com <nageuna922@gmail.com>
2024-08-06 16:27:28 +0900

    <fix>: <반응형 ui 보완>

--- src/components/payment/Checkout.tsx
+++ src/components/payment/Checkout.tsx
@@ -97,7 +97,7 @@ const Checkout = ({
 
   return (
     <div className="outside-modal fixed top-0 left-0 w-screen h-screen bg-Gray-3 bg-opacity-25 z-40 flex justify-center items-center">
-      <div className="w-[320px] md:w-[550px] h-[450px] md:min-h-[600px] p-8 bg-white opacity-100 border border-solid border-Gray-2 rounded-[30px] fixed z-50 flex flex-col overflow-y-scroll">
+      <div className="w-[320px] md:w-[550px] h-[450px] md:h-[620px] p-8 bg-white opacity-100 border border-solid border-Gray-2 rounded-[30px] fixed z-50 flex flex-col overflow-y-scroll scrollbar-hide">
         <h2 className="text-2xl font-bold self-center">스터디 뱃지 포인트 충전</h2>
         {/* 결제 UI, 이용약관 UI 영역 */}
         <div id="payment-widget" />
