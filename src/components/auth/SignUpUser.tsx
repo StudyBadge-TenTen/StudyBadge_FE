e46e99d6cd5de3798c5e1bc84ba4c30c2e4ba53c
nageuna922@gmail.com <nageuna922@gmail.com>
2024-08-10 12:26:17 +0900

    <feat>: <추가 기능 구현 - 계좌 인증>

--- src/components/auth/SignUpUser.tsx
+++ src/components/auth/SignUpUser.tsx
@@ -5,12 +5,54 @@ import { BANK_LIST } from "../../constants/bank-list";
 import PageScrollTop from "../common/PageScrollTop";
 import { CustomErrorType } from "@/types/common";
 import axios from "axios";
+import { getAccountVerification } from "@/services/auth-api";
 
 const SignUp: React.FC = () => {
   const formListFirst = ["이메일", "이름", "비밀번호", "비밀번호확인"];
   const formListSecond = ["계좌번호", "닉네임", "소개"];
+  const [isAccountVerified, setIsAccountVerified] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
   const authStore = useAuthStore();
+
+  const verifyAccount = async () => {
+    const bank = BANK_LIST.find((bankObj) => bankObj.name === authStore.accountBank);
+
+    if (bank && authStore.account) {
+      try {
+        const response = await getAccountVerification(bank.code, authStore.account);
+        if (axios.isAxiosError(response)) {
+          const error = response.response?.data as CustomErrorType;
+          alert(error.message);
+          setIsAccountVerified(() => false);
+        } else {
+          if (response.data.accountHolder === authStore.name) {
+            alert("계좌번호 인증에 성공하였습니다");
+            setIsAccountVerified(() => true);
+          } else {
+            alert("본인 명의의 계좌가 아닙니다. 입력한 이름과 계좌 소유주명이 동일하지 않습니다.");
+            setIsAccountVerified(() => false);
+          }
+        }
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          const customError = error.response?.data as CustomErrorType;
+          alert(customError.message);
+          setIsAccountVerified(() => false);
+        } else {
+          alert(
+            "계좌번호 인증에 문제가 발생하였습니다. 문제가 반복될 경우 studybadge04@gmail.com 해당 주소로 문의 메일을 보내주시면 감사하겠습니다.",
+          );
+          setIsAccountVerified(() => false);
+        }
+      }
+    } else if (!bank) {
+      alert("선택한 은행이 존재하지 않습니다.");
+      return;
+    } else if (!authStore.account) {
+      alert("계좌번호를 입력해주세요");
+      return;
+    }
+  };
 
   const validateForm = (): boolean => {
     let result = true;
@@ -29,6 +71,8 @@ const SignUp: React.FC = () => {
     } else if (!authStore.account) {
       alert("계좌번호 입력은 필수입니다.");
       result = false;
+    } else if (!isAccountVerified) {
+      alert("계좌 인증이 필요합니다");
     } else if (!authStore.password) {
       alert("비밀번호 입력은 필수입니다.");
       result = false;
@@ -40,11 +84,19 @@ const SignUp: React.FC = () => {
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>, formName: string) => {
+    if (formName === "금융기관") {
+      if (isAccountVerified) {
+        setIsAccountVerified(() => false);
+      }
+    }
     if (e.target.id === "계좌번호") {
       if (Number.isNaN(Number(e.target.value))) {
         authStore.setField("account", authStore.account);
         alert("숫자만 입력해주세요");
         return;
+      }
+      if (isAccountVerified) {
+        setIsAccountVerified(() => false);
       }
     }
     authStore.setField(nameToField(formName), e.target.value);
@@ -113,8 +165,8 @@ const SignUp: React.FC = () => {
         >
           <option value="">-- 금융기관을 선택해주세요 --</option>
           {BANK_LIST.map((bank) => (
-            <option key={bank} value={bank}>
-              {bank}
+            <option key={bank.name} value={bank.name}>
+              {bank.name}
             </option>
           ))}
         </select>
@@ -123,10 +175,7 @@ const SignUp: React.FC = () => {
             {formName === "계좌번호" && (
               <div className="w-full max-w-96 text-xs text-Gray-4 mb-2">계좌번호는 환급금 송금을 위한 항목입니다.</div>
             )}
-            <div
-              key={formName}
-              className={`w-full sm:w-96 flex justify-between items-center ${formName === "계좌번호" ? "mb-14" : "mb-6"}`}
-            >
+            <div key={formName} className={`w-full sm:w-96 flex justify-between items-center mb-6`}>
               <label className="text-Blue-2">
                 {formName}
                 {formName !== "소개" && <span>*</span>}
@@ -141,6 +190,14 @@ const SignUp: React.FC = () => {
                 onChange={(e) => handleChange(e, formName)}
               />
             </div>
+            {formName === "계좌번호" &&
+              (isAccountVerified ? (
+                <div className="text-Green-1 mb-14">계좌인증완료</div>
+              ) : (
+                <button onClick={() => verifyAccount()} className="btn-blue mb-14">
+                  계좌번호 인증
+                </button>
+              ))}
           </React.Fragment>
         ))}
         <button type="submit" className="btn-blue hover:bg-blue-700 text-white mt-10">
