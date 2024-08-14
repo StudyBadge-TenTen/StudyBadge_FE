1d640f6ab6c097a22c91fca0db7843a0a2f7c025
nageuna922@gmail.com <nageuna922@gmail.com>
2024-08-14 18:38:36 +0900

    <fix>: <계좌인증 request body 수정>

--- src/services/auth-api.ts
+++ src/services/auth-api.ts
@@ -8,7 +8,7 @@ export const postLogin = async (email: string, password: string): Promise<LoginR
   try {
     const response = await axios.post<LoginResponse>(
       `${API_BASE_URL}/api/members/login`,
-      { email, password },
+      { email: email, password: password },
       { withCredentials: true }, // withCredentials 옵션 추가
     );
 
@@ -107,11 +107,11 @@ export const patchResetPassword = async (email: string, newPassword: string) => 
 export const getAccountVerification = async (bankCode: string, bankNum: string, name?: string) => {
   if (name) {
     await fetchCall<AxiosResponse | AxiosError>(`/api/cert/sign-up`, "get", {
-      bankCode,
-      bankNum,
-      name,
+      bankCode: bankCode,
+      bankNum: bankNum,
+      name: name,
     });
   } else {
-    await fetchCall<AxiosResponse | AxiosError>(`/api/cert/account`, "get", { bankCode, bankNum });
+    await fetchCall<AxiosResponse | AxiosError>(`/api/cert/account`, "get", { bankCode: bankCode, bankNum: bankNum });
   }
 };
