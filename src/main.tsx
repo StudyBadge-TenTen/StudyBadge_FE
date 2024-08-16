ebe3c24152083166478dcf97e7c7542a243dd3b3
nageuna922@gmail.com <nageuna922@gmail.com>
2024-08-16 13:06:30 +0900

    <docs>: <Test 브랜치용으로 msw worker off>

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { worker } from "./mocks/worker.ts";
+// import { worker } from "./mocks/worker.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { setAccessToken, setRefreshToken } from "./utils/cookie.ts";
+// import { setAccessToken, setRefreshToken } from "./utils/cookie.ts";
 
-worker.start();
-setAccessToken(import.meta.env.VITE_APP_ACCESSTOKEN);
-setRefreshToken(import.meta.env.VITE_APP_REFRESHTOKEN);
+// worker.start();
+// setAccessToken(import.meta.env.VITE_APP_ACCESSTOKEN);
+// setRefreshToken(import.meta.env.VITE_APP_REFRESHTOKEN);
 
 const queryClient = new QueryClient();
 
